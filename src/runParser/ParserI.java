79bedbf9d68f1ec988efe128f6d232cd594ce6e0
Cyofanni <giovanni.mazzocchin@studenti.unipd.it>
2017-12-14 16:19:07 +0100

    Main structure for Parser.

--- src/runParser/ParserI.java
+++ src/runParser/ParserI.java
@@ -1,4 +1,17 @@
 package runParser;
+import java.util.*;
+import java.io.*;
+
+class KeyForHashing{
+    private int topic;
+    private String document;
+    public KeyForHashing(int t, String d){
+        topic = t;
+        document = d;
+    }
+}
 
 public interface ParserI {
+    public HashMap<KeyForHashing, Double> readRun(String runFile);
+    // public void normalize(): should accept a score, and a (max,min) couple
 }
