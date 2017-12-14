d794602c231951ce7a2d26389eb4d8fd9e07bf84
Cyofanni <giovanni.mazzocchin@studenti.unipd.it>
2017-12-14 21:23:50 +0100

    Little refactoring.

---
+++ src/runParser/KeyForHashing.java
@@ -0,0 +1,10 @@
+package runParser;
+
+class KeyForHashing{
+    private int topic;
+    private String document;
+    public KeyForHashing(int t, String d){
+        topic = t;
+        document = d;
+    }
+}
