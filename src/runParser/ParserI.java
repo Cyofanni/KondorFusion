33dcd689956a5d169bf83768a6ee286ff0d60955
Cyofanni <giovanni.mazzocchin@studenti.unipd.it>
2017-12-14 21:23:18 +0100

    Little refactoring.

--- src/runParser/ParserI.java
+++ src/runParser/ParserI.java
@@ -2,14 +2,6 @@ package runParser;
 import java.util.*;
 import java.io.*;
 
-class KeyForHashing{
-    private int topic;
-    private String document;
-    public KeyForHashing(int t, String d){
-        topic = t;
-        document = d;
-    }
-}
 
 public interface ParserI {
     public HashMap<KeyForHashing, Double> readRun(String runFile);
