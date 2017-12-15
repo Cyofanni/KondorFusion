338a5c71baf52e2e753f833f6f00d12ea7cc0a96
Cyofanni <giovanni.mazzocchin@studenti.unipd.it>
2017-12-15 20:39:11 +0100

    Parser refactoring.

--- src/runParser/ParserAbs.java
+++ src/runParser/ParserAbs.java
@@ -7,6 +7,11 @@ import java.io.*;
 
 
 public abstract class ParserAbs {
-    abstract protected HashMap<KeyForHashing, Double> readRun(String runFile);
+    protected HashMap<KeyForHashing,Double> linesHash = new HashMap<KeyForHashing,Double>(); //store the lines read from run file
+    protected List<CustomPair<Double, Double>> maxMinPerTopic = new ArrayList();  //stores max and min scores for each topi
+
+    abstract protected void readRun(String runFile);
     abstract protected Double normalizerCaller(Double score, CustomPair<Double,Double> cp);  //should accept a score, and a (max,min) couple
+
+    abstract public void readAndNormalize(String runFile);
 }
