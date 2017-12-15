07b902787481f4926439994a43837b89f4af6893
Giovanni Mazzocchin <giovanni.mazzocchin@studenti.unipd.it>
2017-12-15 13:56:46 +0100

    Delete ParserI.java

--- src/runParser/ParserI.java
+++
@@ -1,9 +0,0 @@
-package runParser;
-import java.util.*;
-import java.io.*;
-
-
-public interface ParserI {
-    public HashMap<KeyForHashing, Double> readRun(String runFile);
-    // public void normalize(): should accept a score, and a (max,min) couple
-}
