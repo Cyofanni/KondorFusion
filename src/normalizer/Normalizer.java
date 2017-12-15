b54f2c924cabb27fc57a4e4389d7b26d5ec6962f
Cyofanni <giovanni.mazzocchin@studenti.unipd.it>
2017-12-15 20:53:41 +0100

    Write Normalizer class.

--- src/normalizer/Normalizer.java
+++ src/normalizer/Normalizer.java
@@ -1,4 +1,18 @@
 package normalizer;
 
 public class Normalizer {
+    Double score;
+    Double max;
+    Double min;
+    public Normalizer(Double score, Double max, Double min) {
+        this.score = score;
+        this.max = max;
+        this.min = min;
+    }
+
+    /**implements Lee95's normalization
+     * */
+    public Double normalize(){
+        return (score - min) / (max - min);
+    }
 }
