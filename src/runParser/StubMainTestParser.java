6c08f0b9a4e7436deeaca74009c0caf01819b180
Cyofanni <giovanni.mazzocchin@studenti.unipd.it>
2017-12-15 21:07:58 +0100

    Add stub main to test parser.

---
+++ src/runParser/StubMainTestParser.java
@@ -0,0 +1,10 @@
+package runParser;
+
+/*Stub main to test the parser before its use by the algos */
+
+public class StubMainTestParser {
+    public static void main(String[] args){
+        ParserAbs pa = new Parser();
+        pa.readAndNormalize("../../BM25b0.75_2.res");
+    }
+}
