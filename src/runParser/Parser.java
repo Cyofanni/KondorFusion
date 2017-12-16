a77ab70d94c664d8754dfea7d6b5e7cb90b26ba3
Marco <marco.alf94@gmail.com>
2017-12-16 17:26:32 +0100

    change readRun method

--- src/runParser/Parser.java
+++ src/runParser/Parser.java
@@ -9,63 +9,71 @@ public class Parser extends ParserAbs {
     /**
      * Reads and parses a file containing retrieval results.
      *
-     * @param  runFile  path to run file
+     * @param  runDirectory path to directory run file
      * @return LinkedHashMap containing topic and docs id's as key, and scores as values
      */
     @Override
-    protected void readRun(String runFile){
-        Scanner run = null;
-        try{
-            run = new Scanner(new File(runFile));
+    protected void readRun(String runDirectory){
+        File[] runFiles = new File(runDirectory).listFiles();
+
+        for(int i = 0; i < runFiles.length; i++){
+            Scanner run = null;
+            try{
+                run = new Scanner(runFiles[i]);
+            }
+            catch(FileNotFoundException exc){
+                exc.printStackTrace();
+            }
+
+            Double maxScore = null;
+            Double minScore = null;
+            int oldTop = Integer.MIN_VALUE;   //old topic, used to check if it changes in the next iteration
+            CustomPair<Double, Double> coupleMaxMin = null;
+
+            while(run.hasNextLine()){
+                int top;   //topic
+                String doc;  //document id
+                Double score;  //score
+                top = run.nextInt();
+                run.next();
+                doc = run.next();
+                run.next();
+                score = run.nextDouble();
+
+                if (top != oldTop){
+                    /**if the topic changes, put (max,min) of the previous topic in maxMinPerTopic and reset;
+                     the topic changes trivially also in the first iteration, so put a dummy couple in the
+                     first item of the list and throw it away later
+                     */
+                    coupleMaxMin = new CustomPair<Double,Double>(minScore, maxScore);
+                    maxMinPerTopic.add(coupleMaxMin);
+                    maxScore = Double.NEGATIVE_INFINITY;   //reset for the next topic
+                    minScore = Double.POSITIVE_INFINITY;
+                }
+
+                if (score > maxScore){
+                    maxScore = score;
+                }
+                else if (score < minScore){
+                    minScore = score;
+                }
+
+                KeyForHashing currKey = new KeyForHashing(top, doc);
+                linesHash.put(currKey, score);
+
+                oldTop = top;   //set old topic to current topic
+                run.nextLine();  //mandatory instruction
+            }
+
+            //add the last (max,min) to the list, because it has been skipped by the while loop
+            coupleMaxMin = new CustomPair<>(minScore, maxScore);
+            maxMinPerTopic.add(coupleMaxMin);
+
+            //throw away the first dummy item from maxMinPerTopic
+            maxMinPerTopic.remove(0);
         }
-        catch(FileNotFoundException exc){}
 
-        Double maxScore = null;
-        Double minScore = null;
-        int oldTop = Integer.MIN_VALUE;   //old topic, used to check if it changes in the next iteration
-        CustomPair<Double, Double> coupleMaxMin = null;
 
-        while(run.hasNextLine()){
-            int top;   //topic
-            String doc;  //document id
-            Double score;  //score
-            top = run.nextInt();
-            run.next();
-            doc = run.next();
-            run.next();
-            score = run.nextDouble();
-
-            if (top != oldTop){
-                /**if the topic changes, put (max,min) of the previous topic in maxMinPerTopic and reset;
-                   the topic changes trivially also in the first iteration, so put a dummy couple in the
-                   first item of the list and throw it away later
-                 */
-                coupleMaxMin = new CustomPair<Double,Double>(minScore, maxScore);
-                maxMinPerTopic.add(coupleMaxMin);
-                maxScore = Double.NEGATIVE_INFINITY;   //reset for the next topic
-                minScore = Double.POSITIVE_INFINITY;
-            }
-
-            if (score > maxScore){
-                maxScore = score;
-            }
-            else if (score < minScore){
-                minScore = score;
-            }
-
-            KeyForHashing currKey = new KeyForHashing(top, doc);
-            linesHash.put(currKey, score);
-
-            oldTop = top;   //set old topic to current topic
-            run.nextLine();  //mandatory instruction
-        }
-
-        //add the last (max,min) to the list, because it has been skipped by the while loop
-        coupleMaxMin = new CustomPair<>(minScore, maxScore);
-        maxMinPerTopic.add(coupleMaxMin);
-
-        //throw away the first dummy item from maxMinPerTopic
-        maxMinPerTopic.remove(0);
     }
 
     @Override
