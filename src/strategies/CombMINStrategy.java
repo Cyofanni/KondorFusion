0fa923a4b920b836644dbc174a1929f9c6447bce
marcusNet <marco.alf94@gmail.com>
2017-12-17 17:36:40 +0100

    CombMinStrategy

---
+++ src/strategies/CombMINStrategy.java
@@ -0,0 +1,15 @@
+package strategies;
+
+
+import runParser.Parser;
+import utils.CustomPair;
+
+import java.util.Map;
+
+public class CombMINStrategy {
+
+    public static Map<Integer, CustomPair<String, Double>> combMIN(Parser p){
+        
+
+    }
+}
